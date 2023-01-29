1993ef8910eb5d62a415de45acb8152ba1ccfb27
ITboo <annaacdc@tut.by>
2023-01-29 20:22:34 +0300

    init: start task 2

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,6 +1,9 @@
 import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
 import { graphqlBodySchema } from './schema';
 
+/*Чтобы заставить graphql работать, вам надо импортировать graphql функцию из пакета graphql, передать ей необходимые параметры и вернуть клиенту результат запуска этой функции*/
+import { graphql, GraphQLSchema } from 'graphql';
+
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
   fastify
 ): Promise<void> => {
@@ -11,7 +14,13 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         body: graphqlBodySchema,
       },
     },
-    async function (request, reply) {}
+    async function (request, reply) {
+      const schema: GraphQLSchema = new GraphQLSchema({
+        query: //MyAppQueryRootType,
+        mutation: //MyAppMutationRootType
+      });
+      return await graphql();
+    }
   );
 };
 
