320ffd278bbab65a26fd5d063d86f9a1e8bf03cc
ITboo <annaacdc@tut.by>
2023-01-30 16:33:30 +0300

    feat: add queries

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -2,9 +2,10 @@ import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-sc
 import { graphqlBodySchema } from './schema';
 
 /*Чтобы заставить graphql работать, вам надо импортировать graphql функцию из пакета graphql, передать ей необходимые параметры и вернуть клиенту результат запуска этой функции*/
-import { graphql, GraphQLSchema, validate } from 'graphql';
+import { graphql, GraphQLSchema, /*validate*/ } from 'graphql';
 /*нужно описать какие сущности знает сервер (например user и у него есть такие-то поля), а потом написать т.н. ресолверы (resolvers) которые будут возвращать эти данные по требованию (например все юзеры, или юзер с айди 1)*/
 import { query } from './query';
+import { mutation } from './mutation';
 
 //import  from 'graphql-depth-limit';
 
@@ -37,3 +38,10 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
 };
 
 export default plugin;
+/*сначала вызываются множественные postsLoader.load(id) - хоть 100 штук
+
+под капотом даталодер "накапливает" эти айдишники и передаёт их массивом в функцию, переданную в конструктор даталодера
+
+там, получив массив userIds мы делаем 1 запрос в БД и формируем мапу со структурой, которую ты описала, и в ответе отдаём массив в такой же очередности, как пришли айдишники в userIds (изменено)
+[16:26]
+читать тут https://www.npmjs.com/package/dataloader - Batch function*/
--- src/routes/graphql/query.ts
+++ src/routes/graphql/query.ts
@@ -3,20 +3,76 @@ import {
     GraphQLList
 } from 'graphql';
 import {
-    GraphQLUser
-} from './types/index'
+    GraphQLUser,
+    GraphQLProfile,
+    GraphQLPost,
+    GraphQLMemberType
+} from './types/index';
 
+/* функция resolve() принимает 3 аргумента. Добавь их, и выведи - там третьим аргументов будет фастифай. resolve(parent, args, fastify)*/
 export const query = new GraphQLObjectType({
     name: 'Query',
     fields: {
-        users: {},
-        user: {},
-        profiles: {},
-        profile: {},
-        posts: {},
-        post: {},
-        memberTypes: {},
-        memberType: {}
+        //USER
+        users: {
+            type: new GraphQLList(GraphQLUser),
+            async resolve(parent, args, fastify) { return await fastify.db.users.findMany() }
+        },
+        user: {
+            type: GraphQLUser,
+            async resolve(parent, args, fastify) {
+                const user = await fastify.db.users.findOne({ key: 'id', equals: args.id });
+                if (user === null) {
+                    throw fastify.httpErrors.notFound();
+                }
+                return user;
+            }
+        },
+        // PROFILE
+        profiles: {
+            type: new GraphQLList(GraphQLProfile),
+            async resolve(parent, args, fastify) { return await fastify.db.profiles.findMany() }
+        },
+        profile: {
+            type: GraphQLProfile,
+            async resolve(parent, args, fastify) {
+                const profile = await fastify.db.profiles.findOne({ key: 'id', equals: args.id });
+                if (profile === null) {
+                    throw fastify.httpErrors.notFound();
+                }
+                return profile;
+            }
+        },
+        //POST
+        posts: {
+            type: new GraphQLList(GraphQLPost),
+            async resolve(parent, args, fastify) { return await fastify.db.posts.findMany() }
+        },
+        post: {
+            type: GraphQLPost,
+            async resolve(parent, args, fastify) {
+                const post = await fastify.db.posts.findOne({ key: 'id', equals: args.id });
+                if (post === null) {
+                    throw fastify.httpErrors.notFound();
+                };
+                return post;
+            }
+        },
+        //MEMBER-TYPE
+        memberTypes: {
+            type: new GraphQLList(GraphQLMemberType),
+            async resolve(parent, args, fastify) { return fastify.db.memberTypes.findMany() }
+        },
+        memberType: {
+            type: GraphQLMemberType,
+            async resolve(parent, args, fastify) {
+                const memberType = await fastify.db.memberTypes.findOne({ key: 'id', equals: args.id });
+                if (memberType === null) {
+                    throw fastify.httpErrors.notFound();
+                }
+                return memberType;
+            }
+        }
     }
 }
 );
